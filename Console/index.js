294e7575339d97e3aad5e5b484b60fc82747bdd7
Patel Suhan <psuhan976@gmail.com>
2024-08-08 15:21:35 +0530

    js task

---
+++ Console/index.js
@@ -0,0 +1,12 @@
+const a = (document.getElementById("h2").innerHTML = "This is h2");
+  console.log(a);
+
+  for (let a = 0; a < 10; a++) {
+  console.log(a);
+    
+    
+  }
+
+   a = "jvfasdygvfuysj"
+
+  console.log(a);
